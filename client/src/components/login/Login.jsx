2324e901631fd3b904f41418734e50cef19743e4
Miro1717 <122827724+Miro1717@users.noreply.github.com>
2023-11-18 13:59:20 +0200

    update login html and css

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
-import "./login.css";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Login = function () {
   return (
-    <form>
-      <h3>Login</h3>
-
-      <label for="username">Email</label>
-      <input type="text" placeholder="name@gmail.com" id="username" />
-
-      <label for="password">Password</label>
-      <input type="password" placeholder="Password" id="password" />
-
-      <button>Log In</button>
-
-      <span>
-        If you don't have profile click <Link to="/users/register">here</Link>
-      </span>
-    </form>
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        padding: "10px",
+      }}
+    >
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+      <Button variant="secondary" style={{ margin: "10px 140px" }}>
+        Log In
+      </Button>
+      <p>
+        <span style={{ color: "white", margin: "55px" }}>
+          If you don't have profile click <Link to="/users/register">here</Link>
+        </span>
+      </p>
+    </div>
   );
 };
 
