6e2c2bfd249a7cd025381eb06090a12fb6523c44
Miro1717 <122827724+Miro1717@users.noreply.github.com>
2023-11-18 10:11:22 +0200

    add login page html and css

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -3,45 +3,21 @@ import "./login.css";
 
 const Login = function () {
   return (
-    <section id="login-page">
-      <div className="loginSection">
-        <form method="POST" className="loginForm">
-          <h2>Login</h2>
-          <ul className="noBullet">
-            <li>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="text"
-                className="inputFields"
-                id="email"
-                name="email"
-                placeholder="name@gmail.com"
-              />
-            </li>
-            <li>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                className="inputFields"
-                id="password"
-                name="password"
-                placeholder="*******"
-              />
-            </li>
+    <form>
+      <h3>Login</h3>
 
-            <li id="center-btn">
-              <button id="login-btn">Login</button>
-            </li>
-          </ul>
-        </form>
-        <p className="field">
-          <span>
-            If you don't have profile click{" "}
-            <Link to="/users/register">here</Link>
-          </span>
-        </p>
-      </div>
-    </section>
+      <label for="username">Email</label>
+      <input type="text" placeholder="name@gmail.com" id="username" />
+
+      <label for="password">Password</label>
+      <input type="password" placeholder="Password" id="password" />
+
+      <button>Log In</button>
+
+      <span>
+        If you don't have profile click <Link to="/users/register">here</Link>
+      </span>
+    </form>
   );
 };
 
