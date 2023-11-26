b3de00b5d49f2fc3c53b3eecdcf593d495047178
Miro1717 <122827724+Miro1717@users.noreply.github.com>
2023-11-26 12:57:25 +0200

    add AuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import * as authService from "./services/user/authService";
-import Path from "./path";
+import { AuthProvider } from "./context/authContext";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -16,64 +14,25 @@ import NotFound from "./components/not-found/NotFound";
 import Details from "./components/cocktail-details/CocktailDetails";
 import Edit from "./components/cocktail-edit/CocktailEdit";
 import Logout from "./components/logout/Logout";
-import AuthContext from "./context/authContext";
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem("accessToken");
-    return {};
-  });
-
-  const loginSubmitHandler = async (values) => {
-    const result = await authService.login(values.email, values.password);
-
-    setAuth(result);
-    localStorage.setItem("accessToken", result.accessToken);
-
-    navigate(Path.Home);
-  };
-
-  const registerSubmitHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-    setAuth(result);
-    localStorage.setItem("accessToken", result.accessToken);
-    navigate(Path.Home);
-  };
-
-  const logoutHandler = () => {
-    setAuth({});
-    localStorage.removeItem("accessToken");
-  };
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  };
-
   return (
-    <AuthContext.Provider value={values}>
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cocktails/catalog" element={<Catalog />}></Route>
-          <Route path="/cocktails/create" element={<Create />}></Route>
-          <Route path="/cocktails/edit" element={<Edit />}></Route>
-          <Route path="/cocktails/:id" element={<Details />}></Route>
-          <Route path="/users/profile" element={<MyProfile />}></Route>
-          <Route path="users/login" element={<Login />}></Route>
-          <Route path="users/register" element={<Register />}></Route>
-          <Route path="users/logout" element={<Logout />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </>
-    </AuthContext.Provider>
+    <AuthProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cocktails/catalog" element={<Catalog />}></Route>
+        <Route path="/cocktails/create" element={<Create />}></Route>
+        <Route path="/cocktails/edit" element={<Edit />}></Route>
+        <Route path="/cocktails/:id" element={<Details />}></Route>
+        <Route path="/users/profile" element={<MyProfile />}></Route>
+        <Route path="users/login" element={<Login />}></Route>
+        <Route path="users/register" element={<Register />}></Route>
+        <Route path="users/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </AuthProvider>
   );
 }
 
